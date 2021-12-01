318bb333cffccac4f7ccc69fbc3b3d091fbc4807
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-01 00:16:32 +0000

    sppiner2.js

---
+++ spinner2.js
@@ -0,0 +1,17 @@
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const loading = '\r|\r/\r-\r\\\r|\n';
+// console.log(arr.length);
+let arr = [];
+for (const char of loading) {
+  // console.log(char);
+  arr.push(char);
+  // console.log(arr.length);
+}
+
+for (let i = 0; i < arr.length; i++) {
+  // console.log(arr);
+  setTimeout(() => {
+    process.stdout.write(arr[i]);
+  }, 100 + i * 200);
+}
